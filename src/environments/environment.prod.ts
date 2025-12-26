baf48fa9fff399f4df78e9fc1b7bb493919fbbd0
muhammed-sayeed <sayeedmon25@gmail.com>
2025-12-26 10:14:59 +0530

    url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  userApiUrl: 'http://13.60.173.205',
-  adminApiUrl: 'http://13.60.173.205/admin'
+  userApiUrl: 'https://codforum.onrender.com',
+  adminApiUrl: 'https://codforum.onrender.com/admin'
 };
